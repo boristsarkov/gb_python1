e78aa8cb7da44c938db11739f9eaee66304c4093
boristsarkov <89899909+boristsarkov@users.noreply.github.com>
2021-09-29 11:41:48 +0400

    Update Task_3_3.py

--- Task_3_3.py
+++ Task_3_3.py
@@ -1,1 +1,9 @@
 def thesaurus(*names):
+end_dict = dict()
+    for i in names:
+        end_dict.setdefault(i[0], [])
+        end_dict[i[0]].append(i)
+    return end_dict
+
+
+print(thesaurus('Илья', 'Владимир', 'Василий', 'Николай', 'Иван'))
